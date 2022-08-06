48c62b6804c5f9cd6a1794e59eece4bf6bfcb5bf
Linh <nguyentuanlinh432001@gmail.com>
2022-08-07 02:26:57 +0700

    upload file

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+var menus = [
+    {
+        menu: 'HOME'
+    },
+    {
+        menu: 'ABOUT'
+    },
+    {
+        menu: 'SERVICES'
+    },
+    {
+        menu:'PORTFOLIO'
+    },
+    {
+        menu: 'CONTACT'
+    }
+]
+
+function menuHandler(item){
+    return `<a>${item.menu}</a>`
+}
+var newMenus = menus.map(menuHandler);
+    newMenus.join('')
+
 let navbar = document.querySelector('.navbar')
 document.querySelector('#burger').onclick = () => {
     navbar.classList.toggle('active');
@@ -5,3 +29,13 @@ document.querySelector('#burger').onclick = () => {
 window.onscroll = () => {
     navbar.classList.remove('active') ;
 }
+
+navbar.innerHTML = newMenus
+
+var listNodes = document.querySelectorAll('.navbar a');
+    listNodes[0].href = '#home'
+    listNodes[1].href = '#about'
+    listNodes[2].href ='#services'
+    listNodes[3].href='#portfolio'
+    listNodes[4].className = 'button'
+    listNodes[4].href ='#contact'
